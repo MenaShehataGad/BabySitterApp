7c92fca3242df83f48807aab003567150a9805a9
Mina Gad <mina@Minas-MacBook-Pro.local>
2017-01-28 22:38:59 +0200

    Initial Commit

---
+++ BabySitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BabySitter
+//
+//  Created by Mina Shehata Gad on 1/28/17.
+//  Copyright © 2017 Mina Shehata Gad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
